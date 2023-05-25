4019a309dfba437ea5f3fed452dee91569706037
Sonakshis <85751562+Sonakshis@users.noreply.github.com>
2023-05-26 01:39:12 +0530

    Update index.php

--- 5_9_project_files/shareposts/app/views/pages/index.php
+++ 5_9_project_files/shareposts/app/views/pages/index.php
@@ -1,5 +1,5 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
-  <div class="jumbotron jumbotron-flud text-center">
+  <div class="jumbotron jumbotron-fluid text-center">
     <div class="container">
     <h1 class="display-3"><?php echo $data['title']; ?></h1>
     <p class="lead"><?php echo $data['description']; ?></p>
